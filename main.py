f3f7c0665fa4e5c271f12fc02bc179b0a9b92212
Артём Петрушкин <depetrushkin@yandex.ru>
2023-01-30 12:39:08 +0300

    after change None on  and new line main()

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ def start_training(char_name: str, char_class: str) -> str:
           'defence — чтобы блокировать атаку противника или '
           'special — чтобы использовать свою суперсилу.')
     print('Если не хочешь тренироваться, введи команду skip.')
-    cmd: str = None
+    cmd: str = ''
     while cmd != 'skip':
         cmd = input('Введи команду: ')
         if cmd == 'attack':
@@ -88,3 +88,5 @@ def main():
     print('Воитель, Маг, Лекарь')
     char_class: str = choice_char_class()
     print(start_training(char_name, char_class))
+    
+    main()
